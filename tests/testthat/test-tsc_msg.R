9619d28da532f1496dcd7277513d33bfe730b4f5
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2021-09-29 11:15:58 +0800

    Add tests for tsc_msg_*

--- tests/testthat/test-tsc_msg.R
+++ tests/testthat/test-tsc_msg.R
@@ -5,4 +5,17 @@ test_that("tsc_msg works", {
   testthat::expect_message(tsc_msg_noop("test"))
   testthat::expect_warning(tsc_msg_warn("test"))
   testthat::expect_error(tsc_msg_abort("yo"))
+
+  tsc_setup(verbose = FALSE)
+  testthat::expect_silent(tsc_msg_info("test"))
+  testthat::expect_silent(tsc_msg_success("test"))
+  testthat::expect_silent(tsc_msg_noop("test"))
+  testthat::expect_silent(tsc_msg_warn("test"))
+  testthat::expect_error(tsc_msg_abort("yo"))
+
+  testthat::expect_message(tsc_msg_info("test", verbose = TRUE))
+  testthat::expect_message(tsc_msg_success("test", verbose = TRUE))
+  testthat::expect_message(tsc_msg_noop("test", verbose = TRUE))
+  testthat::expect_warning(tsc_msg_warn("test", verbose = TRUE))
+  testthat::expect_error(tsc_msg_abort("yo"))
 })
